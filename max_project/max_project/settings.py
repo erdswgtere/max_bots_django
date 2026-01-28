20bd6e39976fa2cb7c5f8fe9d088b04cd220e745
erdswgtere <maxxim200508@gmail.com>
2026-01-28 20:42:38 +0300

    feat: добавлены папки для хранения шаблонов и статических файлов

--- max_project/max_project/settings.py
+++ max_project/max_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "max_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,4 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Для collectstatic
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # ← Указываем путь к static
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
