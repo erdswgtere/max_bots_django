ddb79cf70a7814f669696a82c8ed54ae3555d022
erdswgtere <maxxim200508@gmail.com>
2026-02-01 01:15:35 +0300

    feat: замена образов python и postgre

--- max_project/max_project/settings.py
+++ max_project/max_project/settings.py
@@ -89,6 +89,9 @@ DATABASES = {
         "PASSWORD": config('DB_PASSWORD', default='postgres'),
         "HOST": config('DB_HOST', default='localhost'),
         "PORT": config('DB_PORT', default='5432'),
+        "OPTIONS": {
+            "sslmode": "disable",
+        },
     }
 }
 
