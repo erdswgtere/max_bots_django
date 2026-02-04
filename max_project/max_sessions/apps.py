e08b4bc02436db07a44b18784701107f0b54bc89
erdswgtere <maxxim200508@gmail.com>
2026-02-04 14:20:40 +0300

    feat: добавлены сигналы для работы задач запланированных по времени

--- max_project/max_sessions/apps.py
+++ max_project/max_sessions/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class MaxSessionsConfig(AppConfig):
     name = "max_sessions"
+    verbose_name = "Сессии и сообщения"
+
+    def ready(self):
+        import max_sessions.signals
